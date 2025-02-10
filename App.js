46ff91f60c7491b1f50871b316ca5a95e60f0a73
Allison Gonzalez <allison.gonzalez.23s@utzmg.edu.mx>
2025-02-10 08:29:06 -0600

    Final formik
    Practica completa del formulario donde utilizamos formik

--- App.js
+++ App.js
@@ -1,11 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import BasicForm from './components/BasicForm';
+
+// Función de validación
+const validation = (values) => {
+  const errors = {};
+
+  // Validación para el nombre
+  if (!values.name) {
+    errors.name = 'Nombre obligatorio';
+  } else if (values.name.length <= 2) {
+    errors.name = 'Nombre muy corto';
+  }
+
+  // Validación para el apellido
+  if (!values.apellido) {
+    errors.apellido = 'Apellido obligatorio';
+  } else if (values.apellido.length <= 2) {
+    errors.apellido = 'Apellido muy corto';
+  }
+
+  // Validación para el número
+  if (!values.numero) {
+    errors.numero = 'Número obligatorio';
+  } else if (values.numero.length <= 7) {
+    errors.numero = 'Número muy corto';
+  }
+
+  return errors;
+};
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* Pasamos la función de validación como una prop */}
+      <BasicForm validation={validation} />
     </View>
   );
 }
@@ -13,8 +42,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    padding: 20,
   },
 });
